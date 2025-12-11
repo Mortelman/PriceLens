02d0208bc6d39f218798fcdf3a7c9c974edd39a8
Dmitriy Donskoy <donskoydmv@gmail.com>
2025-12-12 00:10:39 +0300

    added basic logger, made RateLimiter class to limit http requests of scrapers, updated WildBerries scraper

--- test_parser.py
+++ test_parser.py
@@ -1,28 +1,34 @@
+import asyncio
 import aiohttp
-import requests
 from abc import ABC, abstractmethod
-from typing import Dict
+from limiter import RateLimiter
 
-WB_PRODUCT_URL = "https://card.wb.ru/cards/v4/detail?appType=1&curr=rub&dest=1259570207&spp=30&hide_vflags=4294967296&hide_dtype=9%3B11&ab_testing=false&lang=ru&nm=" # + article
+from logger import get_logger, full_log
+logger = get_logger('scraper/scraper.py')
+
 
 class BaseScraper(ABC):
     marketplace: str
+    product_url: str
+    image_url: str
+    headers: dict
 
     @abstractmethod
-    async def fetch_product(self, session: aiohttp.ClientSession, url: str) -> Dict:
+    async def fetch_product(self, session: aiohttp.ClientSession, **kwargs) -> dict:
         """
-        Получает данные о товаре по URL:
+        Получает данные о товаре по URL/артикулу:
         {
-            'marketplace': str,
-            'id': str | None,
+            'internal_id': str,
             'name': str,
+            'marketplace': str,
             'brand': str,
             'brand_id': int
-            'price_basic': int | float,
-            'price': int | float,
-            'total_quantity': int,
-            'url': str | None,
-            'image_url': str | None,
+            'price_basic': float,
+            'price': float,
+            'image_url': str,
+            'size': str
+            'quantity': int,
+            'pics': int,
         }
         """
         raise NotImplementedError
@@ -30,10 +36,9 @@ class BaseScraper(ABC):
 
 class WBScraper(BaseScraper):
     marketplace = 'wildberries'
-
-    def __init__(self) -> None:
-        self.api_url = WB_PRODUCT_URL
-        self.headers = {
+    product_url = 'https://card.wb.ru/cards/v4/detail?appType=1&curr=rub&dest=1259570207&spp=30&hide_vflags=4294967296&hide_dtype=9%3B11&ab_testing=false&lang=ru&nm=article'
+    image_url = 'https://basket-article_basket.wbbasket.ru/vol_article_vol/part_article_part/article/images/big/1.webp'
+    headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
             'Accept': '*/*',
             'Accept-Encoding': 'gzip, deflate, br',
@@ -44,93 +49,135 @@ class WBScraper(BaseScraper):
             'Sec-Fetch-Mode': 'cors',
             'Sec-Fetch-Site': 'cross-site',
         }
+    
+    def __init__(self):
+        self._limiter = RateLimiter(
+            period=60, limit=300,
+            interval=0.2, burst=20
+        )
+    
+    @staticmethod
+    def _get_vol_and_part(article: str) -> tuple[str, str]:
+        if int(article) >= 100_000_000:
+            return article[:4], article[:6]
+        else:
+            return article[:3], article[:5]
+    
+    @staticmethod
+    def _get_url(
+        base_url: str,
+        article: str,
+        basket: str = '',
+        vol: str = '',
+        part: str = ''
+    ) -> str:
+        url = base_url
+        url = url.replace('article_basket', basket).replace('_article_vol', vol).replace('_article_part', part).replace('article', article)
+        return url
+    
+    @staticmethod
+    def _get_article(**kwargs) -> str:
+        article = kwargs.get('article', None)
+        if article is None:
+            url = kwargs.get('url', None)
+            if url is None:
+                raise RuntimeError('invalid arguments')
+            article = url.lstrip('https://www.wildberries.ru/catalog/').rstrip('/detail.aspx')
+        return str(article)
 
-    async def fetch_product(self, session: aiohttp.ClientSession, article: int) -> Dict:
-        url = self.api_url + str(article)
+    async def fetch_product(self, session: aiohttp.ClientSession, **kwargs) -> list[dict]:
+        article = self._get_article(**kwargs)
+        url = self._get_url(self.product_url, article)
+        print(url)
         async with session.get(url=url, headers=self.headers) as response:
             status = response.status
-            text = await response.text()
 
             if status == 200:
                 data = await response.json()
-                return await self.parse_product(data)
+                image_url = await self._get_product_image(session, article)
+                product_info = self._parse_product(data, image_url)
+                return product_info
 
+            text = await response.text()
             snippet = text.replace('\n', ' ')
+            full_log(logger=logger, where="/fetch_product")
             raise RuntimeError(f'Unexpected status {status} for {article}: {snippet}')
         
-    async def parse_product(self, data: Dict):
+    def _parse_product(self, data: dict, image_url: str) -> list[dict]:
         product = data['products'][0]
-        id = product['id']
+        internal_id = product['id']
         name = product['name']
         brand = product['brand']
         brand_id = product['brandId']
-        price_basic = product['sizes'][0]['price']['basic'] / 100
-        price = product['sizes'][0]['price']['product'] / 100
-        total_quantity = product['totalQuantity']
+        pics = product['pics']
 
-        return {
-            'marketplace': self.marketplace,
-            'id': id,
-            'name': name,
-            'brand': brand,
-            'brand_id': brand_id,
-            'price_basic': price_basic,
-            'price': price,
-            'total_quantity': total_quantity,
-        }
+        parsed_data = []
+        for size in product['sizes']:
+            size_name = size['name']
+            price_basic, price = 0.0, 0.0
+            quantity = 0
 
-# if __name__ == '__main__':
-#     async def main():
-#         article = 288392979
-#         async with aiohttp.ClientSession() as session:
-#             scraper = WBScraper()
-#             try:
-#                 result = await scraper.fetch_product(session, article)
-#                 print(result)
-#             except Exception as e:
-#                 print('Ошибка:', e)
+            price_dict = size.get('price', None)
+            if price_dict is not None:
+                price_basic = price_dict['basic'] / 100
+                price = price_dict['product'] / 100
+                quantity = size['stocks'][0]['qty']
 
-#     asyncio.run(main())
+            parsed_data.append({
+                'marketplace': self.marketplace,
+                'internal_id': internal_id,
+                'name': name,
+                'brand': brand,
+                'brand_id': brand_id,
+                'price_basic': price_basic,
+                'price': price,
+                'size': size_name,
+                'quantity': quantity,
+                'image_url': image_url,
+                'pics': pics
+            })
 
-def get_vol_and_part(article: int) -> tuple[str, str]:
-    if article >= 100_000_000:
-        return str(article)[:4], str(article)[:6]
-    else:
-        return str(article)[:3], str(article)[:5]
+        return parsed_data
 
-def get_product_image(article: int):
-    vol, part = get_vol_and_part(article)
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
-        "Accept": "*/*",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "en-US,en;q=0.9",
-        "Origin": "https://www.wildberries.ru",
-        "Referer": "https://www.wildberries.ru/",
-        "Sec-Fetch-Dest": "empty",
-        "Sec-Fetch-Mode": "cors",
-        "Sec-Fetch-Site": "cross-site",
-    }
-    for number in range(1, 100):
-        if number < 10:
-            basket = f"0{number}"
-        else:
-            basket = str(number)
+    async def _get_product_image(self, session: aiohttp.ClientSession, article: str) -> str:
+        vol, part = self._get_vol_and_part(article)
+        image_url = ''
+        try:
+            for num in range(1, 100):
+                basket = f'{num:02d}'
+                url = self._get_url(self.image_url, article, basket, vol, part)
 
-        api_url = f"https://basket-{basket}.wbbasket.ru/vol{vol}/part{part}/{article}/images/big/1.webp"
-        response = requests.get(api_url, headers=headers)
-        if response.status_code == 200:
-            content_type = response.headers.get("Content-Type", "")
-            if content_type.startswith("image/"):
-                print(f"✅ Изображение валидно: {content_type}")
-                return True
-            else:
-                print(f"❌ Контент не является изображением: {content_type}")
-        else:
-            print(f"Failed: {response.status_code}")
-    return False
+                if self._limiter:
+                    await self._limiter.acquire()
 
-# article = 251898297
-article = 85999881
-result = get_product_image(article)
-print(result)
+                async with session.get(url, headers=self.headers) as response:
+                    if self._limiter:
+                        await self._limiter.record_response(response.status)
+
+                    if response.status == 200:
+                        content_type = response.headers.get('Content-Type', '')
+                        if content_type.startswith('image/'):
+                            image_url = url
+                            break
+                    #     else:
+                    #         print(f'Content is not an image: {content_type}')
+                    # else:
+                    #     print(f'Failed: {response.status}')
+        except BaseException as e:
+            full_log(logger=logger, where="/_get_product_image")
+            raise e
+        finally:
+            return image_url
+
+
+async def main() -> None:
+    # article = 85999881
+    url = 'https://www.wildberries.ru/catalog/15728047/detail.aspx'
+    scraper = WBScraper()
+    async with aiohttp.ClientSession() as session:
+        product_info = await scraper.fetch_product(session, url=url)
+    for product in product_info:
+        print(product, 2*'\n')
+
+if __name__ == "__main__":
+    asyncio.run(main())
